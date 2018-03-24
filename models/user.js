5156219c355fe7264b4e6e24283d634a4ff305b3
tingshufeng <18230467412@139.com>
2018-03-24 16:24:00 +0800

    添加业务模型，操作数据库模块

---
+++ models/user.js
@@ -0,0 +1,30 @@
+// 加载封装连接池的模块
+const { query } = require('../utilities/db-helper')
+
+exports.findByEmail = (email,callback) => {
+	query('SELECT * FROM `users` WHERE email = ?', [email], (err,results) => {
+		if(err){
+			return callback(err)
+		}
+		callback(null,results[0])
+	})
+}
+
+exports.findByNickname = (nickname,callback) => {
+	query('SELECT * FROM `users` WHERE nickname = ?', [nickname], (err,results) => {
+		if(err){
+			return callback(err)
+		}
+		callback(null,results[0])
+	})
+}
+
+exports.save = (user,callback) => {
+	const strSql = 'INSERT INTO `users` SET ?'
+	query(strSql, user, (err,results) => {
+		if(err){
+			return callback(err)
+		}
+		callback(null,results[0])
+	})
+}
