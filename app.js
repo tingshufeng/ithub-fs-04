dde10752287902805b1c2ada2a4850eecd37b33c
tingshufeng <18230467412@139.com>
2018-03-24 20:06:13 +0800

    安装并配置express-session中间件

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const app = express()
 
 // 加载路由模块
@@ -15,6 +16,18 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 // 开放静态资源
 app.use('/node_modules',express.static('./node_modules/'))
 app.use('/public',express.static('./public/'))
+
+
+// 该插件会为 req 请求对象添加一个成员：req.session 默认是一个对象
+// 这是最简单的配置方式，暂且先不用关心里面参数的含义
+app.use(session({
+  // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密
+  // 目的是为了增加安全性，防止客户端恶意伪造
+  secret: 'itcast',
+  resave: false,
+  saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
+}))
+
 
 // 配置解析表单请求体
 // 挂载路由容器到 app 应用程序中使用生效
